d669551e765ff40de436072959b56591c23fcf00
petersudai <psudai@gmail.com>
2024-02-11 13:33:30 -0800

    hbnb

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 from models.base_model import BaseModel
-import json
-from os import path
 from models.user import User
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.review import Review
+from models.place import Place
+import os.path
+import json
 
 
 class FileStorage:
